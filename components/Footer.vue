9884c339cefe2ddf4e84116d9068793e47d1de27
Essi Suomalainen <essi@wilsoncreative.se>
2021-12-07 12:04:41 +0100

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,25 +1,36 @@
 <template>
-  <footer v-if="data" ref="footer" class="w-full py-40 px-[6vw] antialiased footer">
-    <div class="w-full flex mb-40">
-      <div class="w-3/12"></div>
-    </div>
-    <div class="flex -mx-5">
-      <prismic-rich-text :field="data.companyName" />
-      <prismic-rich-text :field="data.address" />
-      <prismic-rich-text :field="data.creditsText" />
+  <footer v-if="data" ref="footer" class="footer">
+    <div>
+      <companyLogo class="company-logo" />
+      <prismic-rich-text :field="data.data.companyName" class="pl-4 font-extrabold" />
+      <prismic-rich-text :field="data.data.address" class="pl-4"/>
+      <img src="@/assets/EUlogo_v_mono.png" alt="" class="eu-logo" />
+      <prismic-rich-text :field="data.data.creditsText" class="pl-4"/>
     </div>
   </footer>
 </template>
 
 <script>
+import companyLogo from '~/assets/logo_2.svg?inline';
 export default {
+  components: { companyLogo },
   props: ['data'],
- 
 }
 </script>
 
 <style>
-    .footer {
-        background-color: #3e4040;
-    }
+.footer {
+  background-color: #3e4040;
+  color: #e6e4d6;
+}
+
+.company-logo {
+  height: 10rem;
+  max-width: 100%;
+  fill: #e6e4d6;
+}
+.eu-logo {
+  height: 6rem;
+  margin: 1rem;
+}
 </style>
