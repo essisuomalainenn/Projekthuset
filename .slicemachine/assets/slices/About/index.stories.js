d43f106c2dc1a8ff180d33e9eabeceae68f249a3
Essi Suomalainen <essi@wilsoncreative.se>
2021-12-07 15:13:36 +0100

    contact

--- .slicemachine/assets/slices/About/index.stories.js
+++ .slicemachine/assets/slices/About/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"about","items":[],"primary":{"about-title":[{"type":"heading2","text":"Whiteboard real-time synergies","spans":[]}],"text":[{"type":"paragraph","text":"Enim in exercitation proident irure amet. Voluptate cupidatat sint dolor eu ea ut voluptate consequat ex cillum minim ipsum commodo aute nulla.","spans":[]}],"Verksamheter":{"link_type":"Web","url":"https://slicemachine.dev"},"logo":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"about","items":[],"primary":{"about-title":[{"type":"heading2","text":"Syndicate innovative models","spans":[]}],"text":[{"type":"paragraph","text":"Laborum labore excepteur Lorem Lorem nostrud anim laboris dolore ipsum nostrud cillum Lorem esse laborum sunt. Sunt in proident adipisicing sint nostrud. Adipisicing cupidatat sunt in reprehenderit amet incididunt ullamco.","spans":[]}],"Verksamheter":{"link_type":"Web","url":"https://prismic.io"},"logo":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1586952518485-11b180e92764?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
---
+++ slices/Contact/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="section">
+    <div class="contact-title-container">
+      <prismic-image :field="slice.primary.logo" class="contact-logo" />
+      <prismic-rich-text
+        :field="slice.primary.title"
+        class="text-2xl tracking-wide"
+      />
+    </div>
+    <prismic-rich-text :field="slice.primary.description" class="mb-14" />
+    <div
+      v-for="(item, i) in slice.items"
+      :key="`slice-item-${i}`"
+      class="flex flex-wrap flex-row items-center mb-16"
+    >
+      <prismic-image :field="item.image" class="rounded-full w-52" />
+      <div class="text-container ml-8">
+        <prismic-rich-text :field="item.name" class="font-medium text-xl" />
+        <prismic-rich-text :field="item.jobTitle" class="font-medium" />
+
+        <div class="contact-info-wrapper mt-10">
+          <prismic-rich-text :field="item.text" class="font-bold"/>
+          <prismic-rich-text :field="item.phoneNumber" />
+          <prismic-link :field="item.email">{{ item.email.url }}</prismic-link>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+.section {
+  position: relative;
+  background: hsla(53, 24%, 87%, 0.9);
+  color: #111;
+  text-align: center;
+  padding: 1rem;
+  text-align: left;
+  padding-bottom: 3rem;
+}
+
+.contact-title-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 1.5rem;
+}
+
+.contact-logo {
+  width: 4rem;
+  background-color: #3e4040;
+  border-radius: 50%;
+  padding: 0.5rem;
+  margin-right: 1.5rem;
+  fill: white;
+}
+
+.contact-info-wrapper {
+
+}
+
+
+.text-container {
+}
+
+a {
+  color: red;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
